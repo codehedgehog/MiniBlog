27b713564ea37c40e4bd4d39fa14da7e2f8a8256
codehedgehog <codehedgehog@outlook.com>
2016-01-27 18:57:09 -0800

    Updating NuGet packages to the latest ones.

--- Website/app_code/handlers/PostHandler.cs
+++ Website/app_code/handlers/PostHandler.cs
@@ -9,140 +9,140 @@ using System.Web;
 
 public class PostHandler : IHttpHandler
 {
-    public void ProcessRequest(HttpContext context)
-    {
-        Blog.ValidateToken(context);
+	public void ProcessRequest(HttpContext context)
+	{
+		Blog.ValidateToken(context);
 
-        if (!context.User.Identity.IsAuthenticated)
-            throw new HttpException(403, "No access");
+		if (!context.User.Identity.IsAuthenticated)
+			throw new HttpException(403, "No access");
 
-        string mode = context.Request.QueryString["mode"];
-        string id = context.Request.Form["id"];
+		string mode = context.Request.QueryString["mode"];
+		string id = context.Request.Form["id"];
 
-        if (mode == "delete")
-        {
-            DeletePost(id);
-        }
-        else if (mode == "save")
-        {
-            EditPost(id, context.Request.Form["title"], context.Request.Form["excerpt"], context.Request.Form["content"], bool.Parse(context.Request.Form["isPublished"]), context.Request.Form["categories"].Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries));
-        }
-    }
+		if (mode == "delete")
+		{
+			DeletePost(id);
+		}
+		else if (mode == "save")
+		{
+			EditPost(id, context.Request.Form["title"], context.Request.Form["excerpt"], context.Request.Form["content"], bool.Parse(context.Request.Form["isPublished"]), context.Request.Form["categories"].Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries));
+		}
+	}
 
-    private void DeletePost(string id)
-    {
-        Post post = Storage.GetAllPosts().FirstOrDefault(p => p.ID == id);
+	private void DeletePost(string id)
+	{
+		Post post = Storage.GetAllPosts().FirstOrDefault(p => p.ID == id);
 
-        if (post == null)
-            throw new HttpException(404, "The post does not exist");
+		if (post == null)
+			throw new HttpException(404, "The post does not exist");
 
-        Storage.Delete(post);
-    }
+		Storage.Delete(post);
+	}
 
-    private void EditPost(string id, string title, string excerpt, string content, bool isPublished, string[] categories)
-    {
-        Post post = Storage.GetAllPosts().FirstOrDefault(p => p.ID == id);
+	private void EditPost(string id, string title, string excerpt, string content, bool isPublished, string[] categories)
+	{
+		Post post = Storage.GetAllPosts().FirstOrDefault(p => p.ID == id);
 
-        if (post != null)
-        {
-            post.Title = title;
-            post.Excerpt = excerpt;
-            post.Content = content;
-            post.Categories = categories;
-        }
-        else
-        {
-            post = new Post() { Title = title, Excerpt = excerpt, Content = content, Slug = CreateSlug(title), Categories = categories };
-            HttpContext.Current.Response.Write(post.Url);
-        }
+		if (post != null)
+		{
+			post.Title = title;
+			post.Excerpt = excerpt;
+			post.Content = content;
+			post.Categories = categories;
+		}
+		else
+		{
+			post = new Post() { Title = title, Excerpt = excerpt, Content = content, Slug = CreateSlug(title), Categories = categories };
+			HttpContext.Current.Response.Write(post.Url);
+		}
 
-        SaveFilesToDisk(post);
+		SaveFilesToDisk(post);
 
-        post.IsPublished = isPublished;
-        Storage.Save(post);
-    }
+		post.IsPublished = isPublished;
+		Storage.Save(post);
+	}
 
-    private void SaveFilesToDisk(Post post)
-    {
-        foreach (Match match in Regex.Matches(post.Content, "(src|href)=\"(data:([^\"]+))\"(>.*?</a>)?"))
-        {
-            string extension = string.Empty;
-            string filename = string.Empty;
+	private void SaveFilesToDisk(Post post)
+	{
+		foreach (Match match in Regex.Matches(post.Content, "(src|href)=\"(data:([^\"]+))\"(>.*?</a>)?"))
+		{
+			string extension = string.Empty;
+			string filename = string.Empty;
 
-            // Image
-            if (match.Groups[1].Value == "src")
-            {
-                extension = Regex.Match(match.Value, "data:([^/]+)/([a-z]+);base64").Groups[2].Value;
-            }
-            // Other file type
-            else
-            {
-                // Entire filename
-                extension = Regex.Match(match.Value, "data:([^/]+)/([a-z0-9+-.]+);base64.*\">(.*)</a>").Groups[3].Value;
-            }
+			// Image
+			if (match.Groups[1].Value == "src")
+			{
+				extension = Regex.Match(match.Value, "data:([^/]+)/([a-z]+);base64").Groups[2].Value;
+			}
+			// Other file type
+			else
+			{
+				// Entire filename
+				extension = Regex.Match(match.Value, "data:([^/]+)/([a-z0-9+-.]+);base64.*\">(.*)</a>").Groups[3].Value;
+			}
 
-            byte[] bytes = ConvertToBytes(match.Groups[2].Value);
-            string path = Blog.SaveFileToDisk(bytes, extension);
+			byte[] bytes = ConvertToBytes(match.Groups[2].Value);
+			string path = Blog.SaveFileToDisk(bytes, extension);
 
-            string value = string.Format("src=\"{0}\" alt=\"\" ", path);
+			string value = string.Format("src=\"{0}\" alt=\"\" ", path);
 
-            if (match.Groups[1].Value == "href")
-                value = string.Format("href=\"{0}\"", path);
+			if (match.Groups[1].Value == "href")
+				value = string.Format("href=\"{0}\"", path);
 
-            Match m = Regex.Match(match.Value, "(src|href)=\"(data:([^\"]+))\"");
-            post.Content = post.Content.Replace(m.Value, value);
-        }
-    }
+			Match m = Regex.Match(match.Value, "(src|href)=\"(data:([^\"]+))\"");
+			post.Content = post.Content.Replace(m.Value, value);
+		}
+	}
 
-    private byte[] ConvertToBytes(string base64)
-    {
-        int index = base64.IndexOf("base64,", StringComparison.Ordinal) + 7;
-        return Convert.FromBase64String(base64.Substring(index));
-    }
+	private byte[] ConvertToBytes(string base64)
+	{
+		int index = base64.IndexOf("base64,", StringComparison.Ordinal) + 7;
+		return Convert.FromBase64String(base64.Substring(index));
+	}
 
-    public static string CreateSlug(string title)
-    {
-        title = title.ToLowerInvariant().Replace(" ", "-");
-        title = RemoveDiacritics(title);
-        title = RemoveReservedUrlCharacters(title);
+	public static string CreateSlug(string title)
+	{
+		title = title.ToLowerInvariant().Replace(" ", "-");
+		title = RemoveDiacritics(title);
+		title = RemoveReservedUrlCharacters(title);
 
-        if (Storage.GetAllPosts().Any(p => string.Equals(p.Slug, title, StringComparison.OrdinalIgnoreCase)))
-            throw new HttpException(409, "Already in use");
+		if (Storage.GetAllPosts().Any(p => string.Equals(p.Slug, title, StringComparison.OrdinalIgnoreCase)))
+			throw new HttpException(409, "Already in use");
 
-        return title.ToLowerInvariant();
-    }
+		return title.ToLowerInvariant();
+	}
 
-    static string RemoveReservedUrlCharacters(string text)
-    {
-        var reservedCharacters = new List<string>() { "!", "#", "$", "&", "'", "(", ")", "*", ",", "/", ":", ";", "=", "?", "@", "[", "]", "\"", "%", ".", "<", ">", "\\", "^", "_", "'", "{", "}", "|", "~", "`", "+" };
+	static string RemoveReservedUrlCharacters(string text)
+	{
+		var reservedCharacters = new List<string>() { "!", "#", "$", "&", "'", "(", ")", "*", ",", "/", ":", ";", "=", "?", "@", "[", "]", "\"", "%", ".", "<", ">", "\\", "^", "_", "'", "{", "}", "|", "~", "`", "+" };
 
-        foreach (var chr in reservedCharacters)
-        {
-            text = text.Replace(chr, "");
-        }
+		foreach (var chr in reservedCharacters)
+		{
+			text = text.Replace(chr, "");
+		}
 
-        return text;
-    }
+		return text;
+	}
 
-    static string RemoveDiacritics(string text)
-    {
-        var normalizedString = text.Normalize(NormalizationForm.FormD);
-        var stringBuilder = new StringBuilder();
+	static string RemoveDiacritics(string text)
+	{
+		var normalizedString = text.Normalize(NormalizationForm.FormD);
+		var stringBuilder = new StringBuilder();
 
-        foreach (var c in normalizedString)
-        {
-            var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
-            if (unicodeCategory != UnicodeCategory.NonSpacingMark)
-            {
-                stringBuilder.Append(c);
-            }
-        }
+		foreach (var c in normalizedString)
+		{
+			var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+			if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+			{
+				stringBuilder.Append(c);
+			}
+		}
 
-        return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
-    }
+		return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+	}
 
-    public bool IsReusable
-    {
-        get { return false; }
-    }
+	public bool IsReusable
+	{
+		get { return false; }
+	}
 }
